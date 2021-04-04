19c085ebcfdfad5f5187702d145fff88c8c3fb3f
jrdev <astronomi@gmail.com>
2021-04-04 18:48:48 -0300

    Create home page

---
+++ lib/ui/home_page.dart
@@ -0,0 +1,81 @@
+import 'dart:html';
+
+import 'package:flutter/material.dart';
+import 'package:personal_agenda/helpers/contact_helper.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  ContactHelper helper = ContactHelper();
+
+  List<Contact> contacts = List();
+
+
+  @override
+  void initState() {
+    super.initState();
+
+    helper.getAllContacts().then((list) {
+      setState(() {
+        contacts = list;
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Personal Agenda: My Contacts"),
+        backgroundColor: Colors.indigoAccent,
+        centerTitle: true,
+      ),
+      backgroundColor: Colors.indigo,
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(Icons.add),
+        backgroundColor: Colors.indigoAccent,
+      ),
+      body: ListView.builder(
+          padding: EdgeInsets.all(10.0),
+          itemCount: contacts.length,
+          itemBuilder: (context, index) {
+
+          }
+      ),
+    );
+  }
+
+  Widget _contactCard(BuildContext context, int index){
+    return GestureDetector(
+      child: Card(
+        child: Padding(
+          padding: EdgeInsets.all(10.0),
+          child: Row(
+            children: <Widget>[
+              Container(
+                width: 80.0,
+                height: 80.0,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  image: DecorationImage(
+                    image: contacts[index].img != null
+                        ? FileImage(File(contacts[index].img))
+                        : AssetImage("images/user-default.png")
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(left: 10.0),
+              )
+            ],
+          )
+        ),
+      )
+    );
+  }
+}
