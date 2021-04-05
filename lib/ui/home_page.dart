bf6c85f95f0d9bcde7433382bad687ebb9046ea3
jrdev <astronomi@gmail.com>
2021-04-05 00:12:30 -0300

    Saving contact and updating list

--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -3,6 +3,8 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:personal_agenda/helpers/contact_helper.dart';
 
+import 'contact_page.dart';
+
 class HomePage extends StatefulWidget {
   @override
   _HomePageState createState() => _HomePageState();
@@ -17,11 +19,7 @@ class _HomePageState extends State<HomePage> {
   void initState() {
     super.initState();
 
-    helper.getAllContacts().then((list) {
-      setState(() {
-        contacts = list;
-      });
-    });
+    _getAllContacts();
   }
 
   @override
@@ -34,7 +32,9 @@ class _HomePageState extends State<HomePage> {
       ),
       backgroundColor: Colors.indigo,
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: () {
+          _showContactPage();
+        },
         child: Icon(Icons.add),
         backgroundColor: Colors.indigoAccent,
       ),
@@ -90,6 +90,33 @@ class _HomePageState extends State<HomePage> {
               )
             ],
           )),
-    ));
+        ),
+      onTap: (){
+          _showContactPage(contact: contacts[index]);
+      }
+    );
+  }
+  
+  void _showContactPage({ Contact contact }) async {
+    final recContact = await Navigator.push(context,
+      MaterialPageRoute(builder: (context) => ContactPage(contact: contact))
+    );
+
+    if(recContact != null){
+      if(contact != null){
+        await helper.updateContact(recContact);
+      } else {
+        await helper.saveContact(recContact);
+      }
+      _getAllContacts();
+    }
+  }
+
+  void _getAllContacts() {
+    helper.getAllContacts().then((list) {
+      setState(() {
+        contacts = list;
+      });
+    });
   }
 }
